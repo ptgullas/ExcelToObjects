7a179e4c48b5a50663188c2604522709e3524152
ptgullas <4305689+ptgullas@users.noreply.github.com>
2019-03-28 10:30:47 -0400

    Extensions for ExcelWorksheet (for retrieving headers)

---
+++ ExcelToObjects/Extensions/ExcelWorksheetExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using OfficeOpenXml;
+
+namespace ExcelToObjects {
+    public static class ExcelWorksheetExtensions {
+        public static List<string> GetHeaderColumns(this ExcelWorksheet sheet) {
+            List<string> columnNames = new List<string>();
+            foreach (var firstRowCell in sheet.Cells[sheet.Dimension.Start.Row, sheet.Dimension.Start.Column, 1, sheet.Dimension.End.Column]) {
+                columnNames.Add(firstRowCell.Text);
+            }
+            return columnNames;
+        }
+    }
+}
