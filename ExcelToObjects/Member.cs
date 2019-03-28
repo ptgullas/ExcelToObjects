9c1bed408901b7ba253896181972db1017b8c765
ptgullas <4305689+ptgullas@users.noreply.github.com>
2019-03-27 23:49:20 -0400

    Added Member POCO (Spreadsheets will be mapped to this).

---
+++ ExcelToObjects/Member.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExcelToObjects {
+    public class Member {
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string ZipCode { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+    }
+}
