aba7961297b86c9ec734bc9fd9332029278a2654
ptgullas <4305689+ptgullas@users.noreply.github.com>
2019-03-28 08:27:15 -0400

    Added additional (optional) fields.

--- ExcelToObjects/Member.cs
+++ ExcelToObjects/Member.cs
@@ -12,5 +12,9 @@ namespace ExcelToObjects {
         public string ZipCode { get; set; }
         public string Phone { get; set; }
         public string Email { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string MiddleName { get; set; }
+        public string NameSuffix { get; set; }
+
     }
 }
