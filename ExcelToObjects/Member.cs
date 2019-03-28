ca3106d5bc806fd84ea0a0a4b1691fb6068844ab
ptgullas <4305689+ptgullas@users.noreply.github.com>
2019-03-28 08:28:15 -0400

    Added additional optional fields.

--- ExcelToObjects/Member.cs
+++ ExcelToObjects/Member.cs
@@ -4,12 +4,14 @@ using System.Text;
 
 namespace ExcelToObjects {
     public class Member {
+        // REQUIRED:
         public string LastName { get; set; }
         public string FirstName { get; set; }
+        public string ZipCode { get; set; }
+        // OPTIONAL BUT VERY HELPFUL:
         public string Address { get; set; }
         public string City { get; set; }
         public string State { get; set; }
-        public string ZipCode { get; set; }
         public string Phone { get; set; }
         public string Email { get; set; }
         public DateTime DateOfBirth { get; set; }
