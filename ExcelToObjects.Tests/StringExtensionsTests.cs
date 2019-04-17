a6e4caf3134e2dbafdf0ad1a4d17b529facc1a0b
ptgullas <4305689+ptgullas@users.noreply.github.com>
2019-04-17 19:08:18 -0400

    Added IsValidEmail

---
+++ ExcelToObjects.Tests/StringExtensionsTests.cs
@@ -0,0 +1,19 @@
+﻿using ExcelToObjects.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+
+namespace ExcelToObjects.Test {
+    public class StringExtensionsTests {
+
+        [Fact]
+        public void IsValidEmail_ValidEmail_ReturnsTrue() {
+            string emailToTest = "areacode212@gmail.com";
+            bool result = emailToTest.IsValidEmail();
+
+            Assert.True(result);
+        }
+    }
+}
