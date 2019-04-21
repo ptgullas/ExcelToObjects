990d67373f8fbe20825d1a04f69f162163ab91ae
ptgullas <4305689+ptgullas@users.noreply.github.com>
2019-04-21 11:43:12 -0400

    + PadZipCodeWithZeroes, ContainsFullAddressAndNoZip, RemoveInvalidCharactersFromAddress, ReplaceNumberSignInAddressWithApt, EmailIsValid

--- ExcelToObjects.Tests/MemberTests.cs
+++ ExcelToObjects.Tests/MemberTests.cs
@@ -31,8 +31,85 @@ namespace ExcelToObjects.Test {
             string expected = "2289 Broadway New York NY";
 
             Assert.Equal(expected, member.GetFullAddress());
-
         }
+
+        [Fact]
+        public void PadZipCodeWithZeroes_HasLessThanFiveDigits_Pads() {
+            Member member = new Member() {
+                LastName = "Stark",
+                FirstName = "Lyanna",
+                Address = "2289 Broadway",
+                City = "New York",
+                State = "NY",
+                ZipCode = "7345"
+            };
+            string expected = "07345";
+
+            member.PadZipCodeWithZeroes();
+            Assert.Equal(expected, member.ZipCode);
+        }
+
+        [Fact]
+        public void PadZipCodeWithZeroes_HasFiveDigits_DoesNotPad() {
+            Member member = new Member() {
+                LastName = "Stark",
+                FirstName = "Lyanna",
+                Address = "2289 Broadway",
+                City = "New York",
+                State = "NY",
+                ZipCode = "10024"
+            };
+            string expected = "10024";
+
+            member.PadZipCodeWithZeroes();
+            Assert.Equal(expected, member.ZipCode);
+        }
+
+        [Fact]
+        public void ContainsFullAddressAndNoZip_ReturnsTrue() {
+            Member member = new Member() {
+                LastName = "Stark",
+                FirstName = "Lyanna",
+                Address = "2289 Broadway",
+                City = "New York",
+                State = "NY"
+            };
+
+            Assert.True(member.ContainsFullAddressAndNoZip());
+        }
+
+        [Fact]
+        public void RemoveInvalidCharactersFromAddress_RemovesInvalid() {
+            Member member = new Member() {
+                LastName = "Stark",
+                FirstName = "Lyanna",
+                Address = "205 W. 95th St #23",
+                City = "New York",
+                State = "NY"
+            };
+
+            string expected = "205 W 95th St 23";
+
+            member.RemoveInvalidCharactersFromAddress();
+            Assert.Equal(expected, member.Address);
+        }
+
+        [Fact]
+        public void ReplaceNumberSignInAddressWithApt_Replaces() {
+            Member member = new Member() {
+                LastName = "Stark",
+                FirstName = "Lyanna",
+                Address = "205 W. 95th St #23",
+                City = "New York",
+                State = "NY"
+            };
+
+            string expected = "205 W. 95th St Apt 23";
+
+            member.ReplaceNumberSignInAddressWithApt();
+            Assert.Equal(expected, member.Address);
+        }
+
 
     }
 }
--- ExcelToObjects/Member.cs
+++ ExcelToObjects/Member.cs
@@ -18,22 +18,47 @@ namespace ExcelToObjects {
         public string MiddleName { get; set; }
         public string NameSuffix { get; set; }
 
+        public void PadZipCodeWithZeroes() {
+            // if the spreadsheet contained the Zip Code as a number, it may have removed
+            // leading zeroes. This puts them back
+            if ((ZipCode.Length < 5) && (ZipCode.IsNumeric())) {
+                string fmt = "00000.##";
+                int ZipInt = ZipCode.ToInt();
+                ZipCode = ZipInt.ToString(fmt);
+            }
+        }
+
+        public bool ContainsFullAddressAndNoZip() {
+            bool result = false;
+            if (ContainsFullAddress() && string.IsNullOrEmpty(ZipCode)) {
+                result = true;
+            }
+            return result;
+        }
+
         public bool ContainsFullAddress() {
-            if ((!string.IsNullOrEmpty(Address))
-                && (!string.IsNullOrEmpty(City))
-                && (!string.IsNullOrEmpty(State))) {
-                return true;
+            bool result = false;
+            if ((!string.IsNullOrEmpty(Address)) && (!string.IsNullOrEmpty(City)) && (!string.IsNullOrEmpty(State))) {
+                result = true;
             }
-            else {
-                return false;
-            }
+            return result;
         }
 
         public string GetFullAddress() {
             return $"{Address} {City} {State}";
         }
 
+        public void RemoveInvalidCharactersFromAddress() {
+            Address = Address.ReplaceInvalidChars();
+        }
 
+        public void ReplaceNumberSignInAddressWithApt() {
+            Address = Address.Replace("#", "Apt ");
+        }
+
+        public bool EmailIsValid() {
+            return Email.IsValidEmail();
+        }
 
     }
 
