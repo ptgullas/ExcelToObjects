b1ff4d6dc749e1f02993ffcf9fce284b70c1402b
ptgullas <4305689+ptgullas@users.noreply.github.com>
2019-08-10 17:18:27 -0400

    Added MembersInWorksheet class: contains member list & worksheet name

---
+++ ExcelToObjects/MembersInWorksheet.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExcelToObjects {
+    public class MembersInWorksheet {
+        public List<Member> Members { get; set; }
+        public string NewWorksheetName { get; set; }
+
+        public MembersInWorksheet() {
+            Members = new List<Member>();
+            NewWorksheetName = null;
+        }
+    }
+}
